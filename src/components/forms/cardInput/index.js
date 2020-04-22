01e183f61d2ff4d7345d7c368b77251fa89fb8c3
kyjane <jianhua282721@gmail.com>
2020-04-22 22:56:49 +0800

    modified receipt

--- src/components/forms/cardInput/index.js
+++ src/components/forms/cardInput/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import axios from "axios";
+import { withRouter } from 'react-router-dom';
 import ReactCreditCards from './ReactCreditCards.js';
-export default class PaymentForm extends React.Component {
+ class PaymentForm extends React.Component {
   constructor(props){
     super(props)
     this.state = {
@@ -26,15 +28,40 @@ export default class PaymentForm extends React.Component {
   }
   
   handleSubmit = (e) =>{  
-  e.preventDefault();
-    
+    e.preventDefault();
+    if(localStorage.auth_id_token){
+       // fake aws payment API
+      const data ={
+      paymentMethod : 'Master',
+      amount : 250
+        }
+        axios.post('https://3yappv0hpg.execute-api.ap-southeast-1.amazonaws.com/prod/pay', 
+        data, {
+          headers: {
+            'Content-Type' : 'application/json',
+            'Authorization' : localStorage.auth_id_token
+            // 'Authorization' : 'eyJraWQiOiJQZCtjOGtGSzZBSXRnb3RrU2w4dmtkcnIyS0o5eXdSdEVmVzVudmFGOGZBPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJkNmU3ODMyYi1lNTNhLTQyY2MtYTM3NS04MDM5ZWQxYjNhMjIiLCJhdWQiOiIzOG9jcTNvc2N0ZXQydnZ0N2gwM2RqamU5NSIsImV2ZW50X2lkIjoiNDZjNWEwM2EtODU5OS00YjZkLWJiMDgtZjdmODEwNmE1NzM4IiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE1ODc0NzU4NzEsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5hcC1zb3V0aGVhc3QtMi5hbWF6b25hd3MuY29tXC9hcC1zb3V0aGVhc3QtMl9JWnVoaHhTZnciLCJjb2duaXRvOnVzZXJuYW1lIjoidGVzdHVzZXIiLCJleHAiOjE1ODc0Nzk0NzEsImlhdCI6MTU4NzQ3NTg3MX0.VcnMFvxeaNNr6X1bNXYifTX7BkwgZI5qr-H2CgW-CYrDvbvKKJYxIT2riXaYqHz7C-T1gzOcixW2MRIatBMOlS6cOUNGOXCfULfu8R8o8USX-3XSl38gsuePmpVHUjivQmYR1mAQTlsKbqjwrV6rqmJG1AluxKhuffrLY-dmCd87aSI1lLnQ38zk4ycx8a7Sf9ZY_dE_gMOsO25SYWXMw72AWgSFE_chVJs4BTYug_2gwMSVqQMHzqmHvUo0qXCZa6ez6frHXcRyKY8rK-7zqtQdOfVyzJGh9uNqLFEIg9b5t9RPthuVwykUoc0vatP1CgAdr535XKJp5TE10HQZwA'
+          }
+        })
+        .then(res => {
+          this.props.history.push('/Receipt');
+          console.log(res);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+    else{
+      alert("Please login.")
+    }
+   
   }
 
   render() {
     return (
       <div id="PaymentForm" >
         <ReactCreditCards number={this.state.number} name={this.state.name} cvc={this.state.cvv} expiry={this.state.expiry}   />   
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit} style={{paddingBottom:"1%"}} >
         	<input
             type="tel"
             name="number"
@@ -58,9 +85,10 @@ export default class PaymentForm extends React.Component {
             onChange={this.handleInputChange}
             onFocus={this.handleInputFocus}
           />
-          <input type="submit" style={{width:"150px", height:"30px", borderRadius:'3px' }}   />
+          <input type="submit" value="CHECKOUT" style={{width:"150px", height:"30px", borderRadius:'3px' }}   />
         </form>
       </div>
     );
   }
 }
+export default withRouter(PaymentForm)
--- src/components/forms/payment.js
+++ src/components/forms/payment.js
@@ -5,38 +5,32 @@ import PaymentForm from './cardInput/index.js'
 class Payment extends React.Component{
     constructor(props){
         super(props)
-        this.state ={
-            ModalShow: false
-        }
-        this.handleClose = this.handleClose.bind(this);
-        this.handleShow = this.handleShow.bind(this);
+        // this.state ={
+        //     ModalShow: false
+        // }
+        // this.handleClose = this.handleClose.bind(this);
+        // this.handleShow = this.handleShow.bind(this);
     }
-    handleShow =() =>{
-        this.setState({ModalShow: true});
-    }
-    handleClose =()=>{
-        this.setState({ModalShow: false});
-    }
-    renderModal =(para)=>{
-        
-        return (
-            <div>
-            
-            </div>
-        )
-    }
+    // handleShow =() =>{
+    //     this.setState({ModalShow: true});
+    // }
+    // handleClose =()=>{
+    //     this.setState({ModalShow: false});
+    // }
 
     render(){
        // const show = this.state.ModalShow;
         return (
             <div >
             <div >
-            <p>Paypal Express Checkout:</p>
+            <h5>Paypal Express Checkout:</h5>
+            <br/>
             <PaypalExpress  total={this.props.total} />
             </div>
+            <br/>
             <hr/>
             <br/>
-            <p>Easy Checkout:</p>
+            <h5>Easy Checkout:</h5>
             <div >
                 <PaymentForm/>
             </div>
