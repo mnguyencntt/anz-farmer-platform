c5420bb9525dad02a3c714ff35e8ddd8d1ee7bed
Minh Nguyen <m.nguyencntt@gmail.com>
2020-05-02 16:24:23 +0800

    add minHeight for login page

--- src/components/forms/Login.js
+++ src/components/forms/Login.js
@@ -8,26 +8,29 @@ const LoginForm = ({ authenticateUser, isAuthenticatingUser, token }) => {
   const [inputUsername, setInputUsernameValue] = useState('testuser');
   const [inputPassword, setInputPasswordValue] = useState('testpassword');
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const loginStyle = { minHeight: "700px" };
 
   const doesLoginFail = isSubmitted && !isAuthenticatingUser && !token;
   const loginForm = (
     <div className="container">
-      {/* <form onSubmit={this.handleSubmit}> */}
-      <h2>Login</h2>
-      {doesLoginFail && <h4><span className="red darken-1">Login Fail</span></h4>}
-      {isAuthenticatingUser && <h4><span className='blue lighten-1'>Authenticating ...</span></h4>}
-      <label>
-        Username:
-          <input type="text" name="username" value={inputUsername} onChange={e => setInputUsernameValue(e.target.value)} /> 
-      </label>
-      <label>
-        Password:
-          <input type="text" name="password" value={inputPassword} onChange={e => setInputPasswordValue(e.target.value)} /> 
-      </label>
-      <p></p>
-      <input type="submit" onClick={()=>{setIsSubmitted(true); authenticateUser(inputUsername, inputPassword)}} value="Submit" className="waves-effect waves-light btn" />
-      <p></p>
-      {/* </form> */}
+      <div style={loginStyle}>
+        {/* <form onSubmit={this.handleSubmit}> */}
+        <h2>Login</h2>
+        {doesLoginFail && <h4><span className="red darken-1">Login Fail</span></h4>}
+        {isAuthenticatingUser && <h4><span className='blue lighten-1'>Authenticating ...</span></h4>}
+        <label>
+          Username:
+          <input type="text" name="username" value={inputUsername} onChange={e => setInputUsernameValue(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="text" name="password" value={inputPassword} onChange={e => setInputPasswordValue(e.target.value)} />
+        </label>
+        <p></p>
+        <input type="submit" onClick={() => { setIsSubmitted(true); authenticateUser(inputUsername, inputPassword) }} value="Submit" className="waves-effect waves-light btn" />
+        <p></p>
+        {/* </form> */}
+      </div>
     </div>
   );
 
@@ -44,7 +47,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  authenticateUser: (user, passwd) => { dispatch(authenticateUser(user, passwd))}
+  authenticateUser: (user, passwd) => { dispatch(authenticateUser(user, passwd)) }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginForm)
